a9f1ee3d93c698531bbe51e4cb411ed279c6560c
Roman-code-web <selenerg.275@gmail.com>
2023-01-12 13:35:16 -0500

    configuracion de la ruta detalle/id ya envia

--- src/app/componentes/tabla/tabla.component.ts
+++ src/app/componentes/tabla/tabla.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataTableService } from 'src/app/servicios/data-table.service';
 
 @Component({
@@ -8,10 +9,12 @@ import { DataTableService } from 'src/app/servicios/data-table.service';
 })
 export class TablaComponent {
 
-  //declaro constructor para utilizar la data del servicios
-  constructor(public dataservices:DataTableService){}
+  //1.declaro constructor para utilizar la data del servicios
+  //2.router
+  constructor(public dataservices:DataTableService, private router:Router){}
   //funcion para obtener el id del producto y direccionar
   getProductos(id:string){
     //console.log(id)
+    this.router.navigate(['/detalle',id]);
   }
 }
