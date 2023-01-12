37541c84ec451384cd4782a4a1b31db19bd30d51
Roman-code-web <selenerg.275@gmail.com>
2023-01-12 13:40:54 -0500

    not found agregado

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataTableService } from 'src/app/servicios/data-table.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
-export class DetalleComponent {
-
+export class DetalleComponent implements OnInit{
+  //construtor
+  constructor(public dataservices:DataTableService, private route: ActivatedRoute){}
+//implementamos el onInit para obtener el id del url ni bien inicia el componente
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetalleComponent } from './detalle/detalle.component';
 import { ComponentesModule } from '../componentes/componentes.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetalleComponent
+    DetalleComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
