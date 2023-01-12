a4be7c79521861762138c9bdb0482cf481446e3b
Roman-code-web <selenerg.275@gmail.com>
2023-01-12 14:13:14 -0500

    detalles terminados carga datos segun el id

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -8,10 +8,19 @@ import { DataTableService } from 'src/app/servicios/data-table.service';
   styleUrls: ['./detalle.component.css']
 })
 export class DetalleComponent implements OnInit{
-  //construtor
+  //1.variable
+  id!:string;
+  productoEncontrado=[{id:'',descripcion:'',preciocompra:'',precioventa:'',ganancia:'',existencia:''}];
+  //2.construtor
   constructor(public dataservices:DataTableService, private route: ActivatedRoute){}
-//implementamos el onInit para obtener el id del url ni bien inicia el componente
+  //3.implementamos el onInit para obtener el id del url ni bien inicia el componente
   ngOnInit(): void {
-    
+    this.id=String (this.route.snapshot.paramMap.get('id'));
+    this.cargarDatos(this.id);
+  }
+  //4. funcion que retorne los valores de ese id
+  cargarDatos(id:string){
+    this.productoEncontrado= this.dataservices.listaProductos.filter((element) => element.id === id);
+    console.log(this.productoEncontrado);
   }
 }
